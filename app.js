9887fc0fdc87d0ba6b656244d71c7cb4d5a15b6d
mica635mrlv <millaregina635@gmail.com>
2025-09-09 23:16:03 -0300

    script em andamento

--- app.js
+++ app.js
@@ -1,18 +1,30 @@
-// const imagens = fetch(url) caminho para ter acesso ao endpoint onde estão alocadas as imagens (resultado de uma requisição)
-
 'use strict'
+
+const botaoBuscar = document.getElementById('buscar')
+const barraPesquisa = document.getElementById('barra-pesquisa')
+
+// const main = document.createElement('main');
+// document.body.appendChild(main);
 
 async function buscarImagens(raca) { // tudo no computador acontece em série (async falar para a função esperar a API)
     const url = `https://dog.ceo/api/breed/${raca}/images`
-    const response = await fetch (url) // await = falar para o consolog esperar a API
+    const response = await fetch(url) // await = falar para o consolog esperar a API
     const imagens = await response.json() // transpormar a resposta da API em json (não use await em tudo)
-    console.log(imagens) // Temporario
-    return imagens.messagem // funções puras usam return
-
-// Não use console, tem que usar funções puras
-    console.log(imagens) // Promose {<pending} pq o console log não deu tempo para a API responder
-    console.log(imagens.messagem) //para ter todos as url no console
-    console.log(imagens.messagem[5]) //para ter UMA url especifica no console
+    console.log(imagens)
+    return imagens.messagem
 }
 
-buscarImagens('bulldog')
+buscarImagens('bulldog');
+
+// botaoBuscar.addEventListener('click', () => {
+//     const valorBusca = barraPesquisa.value.trim();
+//     main.innerHTML = ''; // Limpa resultados anteriores
+//     if (valorBusca) {
+//         const resultado = document.createElement('p');
+//         resultado.textContent = `Você buscou: ${valorBusca}`;
+//         main.appendChild(resultado);
+
+//     } else {
+//         prompt('Digite o nome de uma raça!');
+//     }
+// });
